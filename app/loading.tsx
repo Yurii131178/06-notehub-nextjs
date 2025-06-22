949d1ae8c2dc114a899f38819122dcb4ca3a1a4b
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-06-22 13:14:42 +0300

    uncomment my lovely spinner, jsx optimization

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,8 +3,8 @@ import styles from './loading.module.css';
 const Loading = () => {
   return (
     <div className={styles.wrapper}>
-      {/* <div className={styles.loader}></div> */}
-      <p className={styles.text}>Loading, please wait...</p>;
+      <div className={styles.loader}></div>
+      <p className={styles.text}>Loading, please wait...</p>
     </div>
   );
 };
